68e9529a9b262600fb896d10998a32a32a89fe11
tong <sdk@disktree.net>
2011-02-04 06:00:35 +0100

    added omnibox

---
+++ chrome/Omnibox.hx
@@ -0,0 +1,14 @@
+package chrome;
+
+private typedef SuggestResult = {
+	var content : String;
+	var description : String;
+}
+
+@:native("chrome.omnibox") extern class Omnibox {
+	static function setDefaultSuggestion( suggestion : Dynamic ) : Void;
+	static var onInputCancelled(default,null) : Event<Void->Void>;
+	static var onInputChanged(default,null) : Event<String->(Array<SuggestResult>->Void)->Void>;
+	static var onInputEntered(default,null) : Event<String->Void>;
+	static var onInputStarted(default,null) : Event<Void->Void>;
+}
