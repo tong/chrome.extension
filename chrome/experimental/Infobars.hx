a609fa5e20bcb9f26ac72edac9f6c57fb529cbf2
tong <sdk@disktree.net>
2011-02-25 02:54:16 +0100

    minor fix

--- chrome/experimental/Infobars.hx
+++ chrome/experimental/Infobars.hx
@@ -1,5 +1,6 @@
 package chrome.experimental;
 
 @:native("chrome.experimental.infobars") extern class Infobars {
-	static function show( details : Dynamic, cb : Window->Void ) : Void;
+	static function show( details : { tabId : Int, path : String },
+						  ?cb : chrome.Window->Void ) : Void;
 }
