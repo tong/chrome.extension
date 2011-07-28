60814cc70f4c6536e08037ce05a49d82ce1441f6
tong <sdk@disktree.net>
2011-07-29 00:07:55 +0200

    minor fix

--- chrome/ExtensionConnection.hx
+++ chrome/ExtensionConnection.hx
@@ -9,24 +9,17 @@ class ExtensionConnection {
 	
 	/** Extension sends data callback */
 	public dynamic function onMessage( t : String ) {}
-	/** */
-	//public var connected(default,null) : Bool;
 	
 	var div : Dynamic; // js.HtmlDom;
 	var event : js.Event;
 	
-	/**
-	*/
 	public function new( id : String, divName : String = "ext_com" ) {
 		event = untyped document.createEvent( 'Event' );
 		untyped event.initEvent( 'chrome.site.event', true, true );
 		div = js.Lib.document.getElementById( divName );
 		div.addEventListener( 'chrome.extension.event', handleContentScriptEvent );
-		connected = false;
 	}
 	
-	/**
-	*/
 	public function send( t : String ) {
 		div.innerText = t;
 		div.dispatchEvent( event );
