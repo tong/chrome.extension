7cbe2a0c873c4231f02796134d302e8c9f8ee9ae
tong <sdk@disktree.net>
2011-09-23 20:40:26 +0200

    just sync

--- chrome/experimental/Infobars.hx
+++ chrome/experimental/Infobars.hx
@@ -1,6 +1,5 @@
 package chrome.experimental;
 
 @:native("chrome.experimental.infobars") extern class Infobars {
-	static function show( details : { tabId : Int, path : String },
-						  ?cb : chrome.Window->Void ) : Void;
+	static function show( details : { tabId : Int, path : String }, ?cb : chrome.Window->Void ) : Void;
 }
