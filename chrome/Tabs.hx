7b71f22b6893e17c08c4a5f6a6c282fa33b3f994
tong <sdk@disktree.net>
2011-09-30 05:05:43 +0200

    added typedefs for chrome.Tabs event listener arguments

--- chrome/Tabs.hx
+++ chrome/Tabs.hx
@@ -1,5 +1,35 @@
 package chrome;
 
+typedef AttachInfo = {
+	var newWindowId : Int;
+	var newPosition : Int;
+}
+
+typedef DetachInfo = {
+	var oldWindowId : Int;
+	var oldPosition : Int;
+}
+
+typedef MoveInfo = {
+	var windowId : Int;
+	var fromIndex : Int;
+	var toIndex : Int;
+}
+
+typedef RemoveInfo = {
+	var isWindowClosing : Bool;
+}
+
+typedef SelectInfo = {
+	var windowId : Int;
+}
+
+typedef ChangeInfo = {
+	var status : String;
+	var url : String;
+	var pinned : Bool;
+}
+
 @:native("chrome.tabs") extern class Tabs {
 	static function captureVisibleTab( windowId : Int, options : Dynamic, cb : String->Void ) : Void;
 	static function connect( tabId : Int, ?connectInfo : Dynamic ) : Port;
@@ -15,11 +45,11 @@ package chrome;
 	static function remove( tabId : Int, ?cb : Void->Void ) : Void;
 	static function sendRequest( tabId : Int, any : Dynamic, responseCallback : Dynamic->Void ) : Void;
 	static function update( tabId : Int, updateProperties : Dynamic, ?cb : Tab->Void ) : Void;
-	static var onAttached(default,null) : Event<Int->Dynamic->Void>;
+	static var onAttached(default,null) : Event<Int->AttachInfo->Void>;
 	static var onCreated(default,null) : Event<Tab->Void>;
-	static var onDetached(default,null) : Event<Int->Dynamic->Void>;
-	static var onMoved(default,null) : Event<Int->Dynamic->Void>;
-	static var onRemoved(default,null) : Event<Int->Dynamic->Void>;
-	static var onSelectionChanged(default,null) : Event<Int->Dynamic->Void>;
-	static var onUpdated(default,null) : Event<Int->Dynamic->Tab->Void>;
+	static var onDetached(default,null) : Event<Int->DetachInfo->Void>;
+	static var onMoved(default,null) : Event<Int->MoveInfo->Void>;
+	static var onRemoved(default,null) : Event<Int->RemoveInfo->Void>;
+	static var onSelectionChanged(default,null) : Event<Int->SelectInfo->Void>;
+	static var onUpdated(default,null) : Event<Int->ChangeInfo->Tab->Void>;
 }
