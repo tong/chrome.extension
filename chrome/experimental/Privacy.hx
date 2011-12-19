6525e167d19e4ed6222b861ce116c0bbf93b5d34
tong <sdk@disktree.net>
2011-12-19 01:26:06 +0100

    updated experimental privacy api

--- chrome/experimental/Privacy.hx
+++ chrome/experimental/Privacy.hx
@@ -3,8 +3,26 @@ package chrome.experimental;
 import chrome.type.ChromeSetting;
 
 @:native("chrome.experimental.privacy") extern class Privacy {
-	static var network : { networkPredictionEnabled : ChromeSetting };
-	static var services : Dynamic;
-	static var websites : Dynamic;
+	
+	static var network : {
+		networkPredictionEnabled : ChromeSetting
+	};
+	
+	static var services : {
+		alternateErrorPagesEnabled : ChromeSetting,
+		autofillEnabled : ChromeSetting,
+		instantEnabled : ChromeSetting,
+		metricsReportingEnabled : ChromeSetting,
+		safeBrowsingEnabled : ChromeSetting,
+		searchSuggestEnabled : ChromeSetting,
+		translationServiceEnabled : ChromeSetting
+	};
+	
+	static var websites : {
+		thirdPartyCookiesAllowed : ChromeSetting,
+		hyperlinkAuditingEnabled : ChromeSetting,
+		referrersEnabled : ChromeSetting
+	};
+	
 }
 
