5a49d35f7ef3570d3dd359c27c1508791c03b03e
tong <sdk@disktree.net>
2012-01-11 09:34:13 +0100

    optional argument type fix

--- chrome/Extension.hx
+++ chrome/Extension.hx
@@ -16,7 +16,7 @@ private typedef FetchProperties = {
 	static function getViews( ?fetchProperties : FetchProperties ) : Array<js.Window>;
 	static function isAllowedFileSchemeAccess( cb : Bool->Void ) : Void;
 	static function isAllowedIncognitoAccess( cb : Bool->Void ) : Void;
-	static function sendRequest( ?extensionId : String, request : Dynamic, responseCallback : Dynamic->Void ) : Void;
+	static function sendRequest( ?extensionId : String, request : Dynamic, ?responseCallback : Dynamic->Void ) : Void;
 	static var onConnect(default,null) : chrome.Event<Port->Void>;
 	static var onConnectExternal(default,null) : chrome.Event<Port->Void>;
 	static var onRequest(default,null) : chrome.Event<Dynamic->MessageSender->(Dynamic->Void)->Void>;
