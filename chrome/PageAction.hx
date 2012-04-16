c97d6572a0d46a3140f5f08d9ec6c54e4355c618
tong <sdk@disktree.net>
2012-04-16 14:05:31 +0200

    updated PageAction

--- chrome/PageAction.hx
+++ chrome/PageAction.hx
@@ -1,10 +1,36 @@
 package chrome;
 
+/**
+	http://code.google.com/chrome/extensions/pageAction.html
+*/
 @:native("chrome.pageAction") extern class PageAction {
+	
 	static function show( tabId : Int ) : Void;
+	
 	static function hide( tabId : Int ) : Void;
-	static function setIcon( details : Dynamic ) : Void;
-	static function setPopup( details : Dynamic ) : Void;
-	static function setTitle( details : Dynamic ) : Void;
+	
+	static function setIcon(
+		details : {
+			tabId : Int,
+			?imageData : Dynamic, // TODO ImageData
+			?path : String,
+			?iconIndex : Int
+		}
+	) : Void;
+	
+	static function setPopup(
+		details : {
+			tabId : Int,
+			popup : String
+		}
+	) : Void;
+	
+	static function setTitle(
+		details : {
+			tabId : Int,
+			title : String
+		}
+	) : Void;
+	
 	static var onClicked(default,null) : Event<Tab->Void>;
 }
