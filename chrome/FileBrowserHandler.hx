126d0f16d871e5e6cdc5d637325009ca38bb17b6
tong <sdk@disktree.net>
2012-04-16 12:44:13 +0200

    FileBrowserHandler typo fix

--- chrome/FileBrowserHandler.hx
+++ chrome/FileBrowserHandler.hx
@@ -2,12 +2,13 @@ package chrome;
 
 typedef FileHandlerExecuteEventDetails = {
 	var entries : Array<Dynamic>;
-	var tab_id : Int;
+	var tab_id : Null<Int>;
 }
 
 /**
 	This API works only on Chrome OS
+	http://code.google.com/chrome/extensions/fileBrowserHandler.html
 */
 @:native("chrome.fileBrowserHandler") extern class FileBrowserHandler {
-	static var onExecute(default,null) : Event<String->FileHandlerExecuteEventDetails->Void>;
+	static var onExecute(default,never) : Event<String->FileHandlerExecuteEventDetails->Void>;
 }
