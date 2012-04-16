cd366b763edfd6e8cebc141b7b99570c609e4a4f
tong <sdk@disktree.net>
2012-04-16 12:11:13 +0200

    ContextMenus fix

--- chrome/ContextMenus.hx
+++ chrome/ContextMenus.hx
@@ -1,8 +1,44 @@
 package chrome;
 
+typedef OnClickData = {
+	menuItemId : Int,
+	?parentMenuItemId : Int,
+	?mediaType : String,
+	?linkUrl : String,
+	?srcUrl : String,
+	pageUrl : String,
+	?frameUrl : String,
+	?selectionText : String,
+	editable : String
+}
+
 @:native("chrome.contextMenus") extern class ContextMenus {
-	static function create(  createProperties : Dynamic, ?cb : Void->Void ) : Int;
-	static function remove(  menuItemId : Int, ?cb : Void->Void ) : Void;
+	
+	static function create( 
+		createProperties : {
+			?type : String,
+			?title : String,
+			?checked : Bool,
+			?contexts : String,
+			?onclick : OnClickData->Tab->Void,
+			?parentId : Int,
+			?documentUrlPatterns : Array<String>,
+			?targetUrlPatterns : Array<String>
+		},
+		?cb : Void->Void
+	) : Int;
+	
+	static function remove(
+		menuItemId : Int,
+		?cb : Void->Void
+	) : Void;
+	
 	static function removeAll( ?cb : Void->Void ) : Void;
-	static function update(  id : Int, updateProperties : Dynamic, ?cb : Void->Void ) : Void;
+	
+	static function update(
+		id : Int,
+		updateProperties : Dynamic,
+		?cb : Void->Void
+	) : Void;
+	
 }
