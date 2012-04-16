ca23f7fd8787c184a1d21cd6d58e0a9f2878171e
tong <sdk@disktree.net>
2012-04-16 16:32:56 +0200

    updated WebNavigation

--- chrome/WebNavigation.hx
+++ chrome/WebNavigation.hx
@@ -1,44 +1,115 @@
 package chrome;
 
+
+@:fakeEnum(String) enum TransitionType {
+	link;
+	typed;
+	auto_bookmark;
+	auto_subframe;
+	manual_subframe;
+	generated;
+	start_page;
+	form_submit;
+	reload;
+	keyword;
+	keyword_generated;
+}
+
+@:fakeEnum(String) enum TransitionQualifiers {
+	client_redirect;
+	server_redirect;
+	forward_back;
+	from_address_bar;
+}
+
 private typedef GetAllFramesDetails = {
-	var tabId : Int;
+	errorOccurred : Bool,
+	frameId : Int,
+	url : String
 }
 
-private typedef GetFrameDetails = {>GetAllFramesDetails,
-	var frameId : Int;
+private typedef GetFramesDetails = {
+	errorOccurred : Bool,
+	url : String
 }
 
-private typedef WebNavigationEvent = {
-	var tabId : Int;
-	var url : String;
-	var frameId : Int;
-	var timeStamp : Float;
+private typedef OnBeforeNavigateEventDetails = {
+	tabId : Int,
+	url : String,
+	frameId : Int,
+	timeStamp : Float
 }
 
-private typedef WebNavigationCommittedEvent = {>WebNavigationEvent,
-	var transitionType : String; // enumerated string ["link", "typed", "auto_bookmark", "auto_subframe", "manual_subframe", "generated", "start_page", "form_submit", "reload", "keyword", "keyword_generated"]
-	var transitionQualifiers : Array<Dynamic>; //TODO
+private typedef OnCommittedEventDetails = {
+	tabId : Int,
+	url : String,
+	frameId : Int,
+	transitionType : TransitionType,
+	transitionQualifiers : TransitionQualifiers,
+	timeStamp : Float
 }
 
-private typedef WebNavigationCreatedNavigationEvent = {
-	var tabId : Int;
-	var url : String;
-	var frameId : Int;
-	var timeStamp : Float;
+private typedef OnCompletedEventDetails = {
+	tabId : Int,
+	url : String,
+	frameId : Int,
+	timeStamp : Float
 }
 
-private typedef WebNavigationErrorEvent = {>WebNavigationEvent,
-	var error : String;
+private typedef OnCreatedNavigationTargetEventDetails = {
+	sourceTabId : Int,
+	sourceFrameId : Int,
+	url : String,
+	tabId : Int,
+	timeStamp : Float
+}
+
+private typedef OnDOMContentLoadedEventDetails = {
+	tabId : Int,
+	url : String,
+	frameId : Int,
+	timeStamp : Float
+}
+
+private typedef OnErrorOccurredEventDetails = {
+	tabId : Int,
+	url : String,
+	frameId : Int,
+	error : String,
+	timeStamp : Float
+}
+
+private typedef OnReferenceFragmentUpdatedEventDetails = {
+	tabId : Int,
+	url : String,
+	frameId : Int,
+	transitionType : TransitionType,
+	transitionQualifiers : TransitionQualifiers,
+	timeStamp : Float
 }
 
 @:native("chrome.webNavigation") extern class WebNavigation {
-	static function getAllFrames( details : GetAllFramesDetails, cb : Array<Dynamic>->Void ) : Void;
-	static function getFrame( details : GetFrameDetails, cb : Dynamic->Void ) : Void;
-	static var onBeforeNavigate : Event<WebNavigationEvent->Void>;
-	static var onCommitted : Event<WebNavigationCommittedEvent->Void>;
-	static var onCompleted : Event<WebNavigationEvent->Void>;
-	static var onCreatedNavigationTarget : Event<WebNavigationCreatedNavigationEvent->Void>;
-	static var onDOMContentLoaded : Event<WebNavigationEvent->Void>;
-	static var onErrorOccurred : Event<WebNavigationErrorEvent->Void>;
-	static var onReferenceFragmentUpdated : Event<WebNavigationCommittedEvent->Void>;
+	
+	static function getAllFrames(
+		details : {
+			tabId : Int
+		},
+		cb : ?Array<GetAllFramesDetails>->Void
+	) : Void;
+	
+	static function getFrame(
+		details : {
+			tabId : Int,
+			frameId : Int
+		},
+		cb : ?GetFramesDetails->Void
+	) : Void;
+	
+	static var onBeforeNavigate(default,null) : Event<OnBeforeNavigateEventDetails->Void>;
+	static var onCommitted(default,null) : Event<OnCommittedEventDetails->Void>;
+	static var onCompleted(default,null) : Event<OnCompletedEventDetails->Void>;
+	static var onCreatedNavigationTarget(default,null) : Event<OnCreatedNavigationTargetEventDetails->Void>;
+	static var onDOMContentLoaded(default,null) : Event<OnDOMContentLoadedEventDetails->Void>;
+	static var onErrorOccurred(default,null) : Event<OnErrorOccurredEventDetails->Void>;
+	static var onReferenceFragmentUpdated(default,null) : Event<OnReferenceFragmentUpdatedEventDetails->Void>;
 }
