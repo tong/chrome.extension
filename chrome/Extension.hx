8edb2e3d377263997160c74f36b34bbf598f756b
tong <sdk@disktree.net>
2012-04-16 10:56:02 +0200

    chrome.Extension fix

--- chrome/Extension.hx
+++ chrome/Extension.hx
@@ -2,11 +2,9 @@ package chrome;
 
 import js.Dom;
 
-private typedef FetchProperties = {
-	var type : String; // optional // enumerated string ["tab", "infobar", "notification", "popup"] 
-	var windowId : Int; // optional
-}
-
+/**
+	http://code.google.com/chrome/extensions/extension.html
+*/
 @:native("chrome.extension") extern class Extension {
 	
 	static var lastError : { message : String };
@@ -15,11 +13,12 @@ private typedef FetchProperties = {
 	static function connect( extensionId : String, ?connectInfo : { name : String } ) : Port;
 	static function getBackgroundPage() : Dynamic; //js.Window; // DOMWindow
 	static function getURL( path : String ) : String;
-	static function getViews( ?fetchProperties : FetchProperties ) : Array<js.Window>;
+	static function getViews( ?fetchProperties : { ?type : String, ?windowId : Int } ) : Array<js.Window>; // type : enumerated string ["tab", "infobar", "notification", "popup"] 
 	static function isAllowedFileSchemeAccess( cb : Bool->Void ) : Void;
 	static function isAllowedIncognitoAccess( cb : Bool->Void ) : Void;
-	static function sendRequest( ?extensionId : String, request : Dynamic, ?responseCallback : Dynamic->Void ) : Void;
-	
+	@:overload(function( ?extensionId : String, request : Dynamic, ?responseCallback : Dynamic->Void ) : Void {} )
+	static function sendRequest( request : Dynamic, ?responseCallback : Dynamic->Void ) : Void;
+
 	static var onConnect(default,null) : chrome.Event<Port->Void>;
 	static var onConnectExternal(default,null) : chrome.Event<Port->Void>;
 	static var onRequest(default,null) : chrome.Event<Dynamic->MessageSender->(Dynamic->Void)->Void>;
