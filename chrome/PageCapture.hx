59f9ee62bc9ce9b619a55cf825111538a7355388
tong <sdk@disktree.net>
2012-04-16 14:07:12 +0200

    updated PageCapture

--- chrome/PageCapture.hx
+++ chrome/PageCapture.hx
@@ -1,5 +1,8 @@
 package chrome.devtools;
 
+/**
+ * http://code.google.com/chrome/extensions/pageCapture.html
+ */
 @:native("chrome.input.pageCapture") extern class PageCapture {
-	static function saveAsMHTML( details : Dynamic, cb : Blob->Void ) : Void;
+	static function saveAsMHTML( details : { tabId : Int, }, cb : Blob->Void ) : Void;
 }
