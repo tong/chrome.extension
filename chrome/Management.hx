f16f3ae43a7131b71a7df6ee7a4fef4d3a432e57
tong <sdk@disktree.net>
2012-04-16 13:58:30 +0200

    updated Management

--- chrome/Management.hx
+++ chrome/Management.hx
@@ -4,32 +4,42 @@ typedef IconInfo = {
 	var size : Int;
 	var url : String;
 }
-	
+
+@:fakeEnum(String) enum DisabledReason {
+	unknown;
+	permissions_increase;
+}
+
 typedef ExtensionInfo = {
 	var id : String;
 	var name : String;
 	var version : String;
 	var enabled : Bool;
-	var disabledReason : String;
+	var disabledReason : Null<DisabledReason>;
 	var isApp : Bool;
-	var appLaunchUrl : String;
-	var homepageUrl : String;
-	var updateUrl : String;
+	var appLaunchUrl : Null<String>;
+	var homepageUrl : Null<String>;
+	var updateUrl : Null<String>;
 	var offlineEnabled : Bool;
 	var optionsUrl : String;
-	var icons : Array<IconInfo>;
+	var icons : Null<Array<IconInfo>>;
 	var permissions : Array<String>;
 	var hostPermissions : Array<String>;
 }
 
+/**
+	http://code.google.com/chrome/extensions/management.html
+*/
 @:native("chrome.management") extern class Management {
-	static function get( id : String, cb : ExtensionInfo->Void ) : Void;
-	static function getAll( cb : ExtensionInfo->Void ) : Void;
+	
+	static function get( id : String, ?cb : ExtensionInfo->Void ) : Void;
+	static function getAll( ?cb : ExtensionInfo->Void ) : Void;
 	static function getPermissionWarningsById( id : String, ?cb : Array<String>->Void ) : Void;
 	static function getPermissionWarningsByManifest( manifestStr : String, ?cb : Array<String>->Void ) : Void;
 	static function launchApp( id : String, ?cb : Void->Void ) : Void;
 	static function setEnabled( id : String, enabled : Bool, ?cb : Void->Void ) : Void;
 	static function uninstall( id : String, ?cb : Void->Void ) : Void;
+	
 	static var onDisabled(default,null) : Event<ExtensionInfo->Void>;
 	static var onEnabled(default,null) : Event<ExtensionInfo->Void>;
 	static var onInstalled(default,null) : Event<ExtensionInfo->Void>;
