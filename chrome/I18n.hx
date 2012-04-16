9cd0d3a614ceb0c360cc750b8d72509758b84050
tong <sdk@disktree.net>
2012-04-16 13:01:50 +0200

    I18N fix

--- chrome/I18n.hx
+++ chrome/I18n.hx
@@ -1,6 +1,10 @@
 package chrome;
 
+/**
+	http://code.google.com/chrome/extensions/i18n.html
+*/
 @:native("chrome.i18n") extern class I18n {
 	static function getAcceptLanguages( cb : Array<String>->Void ) : Void;
-	static function getMessage( messageName : String, ?substitutions : Dynamic ) : String;
+	@:overload(function( messageName : String, ?substitutions : Array<String> ) : Void {} )
+	static function getMessage( messageName : String, ?substitutions : String ) : String;
 }
