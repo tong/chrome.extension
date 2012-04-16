1c52fb65fe79e48e50d5e4cc7406ec5226de2fab
tong <sdk@disktree.net>
2012-04-16 14:11:58 +0200

    updated Permissions

--- chrome/Permissions.hx
+++ chrome/Permissions.hx
@@ -1,15 +1,20 @@
 package chrome;
 
 typedef PermissionsData = {
-	var permissions : Array<String>;
-	var origins : Array<String>;
+	?permissions : Array<String>,
+	?origins : Array<String>
 }
 
+/**
+ * http://code.google.com/chrome/extensions/permissions.html
+ */
 @:native("chrome.permissions") extern class Permissions {
+	
 	static function contains( permissions : PermissionsData, cb : Bool->Void ) : Void;
 	static function getAll( permissions : PermissionsData ) : Void;
 	static function remove( permissions : PermissionsData, ?cb : Bool->Void ) : Void;
 	static function request( permissions : PermissionsData, ?cb : Bool->Void ) : Void;
+	
 	static var onAdded : Event<PermissionsData->Void>;
 	static var onRemoved : Event<PermissionsData->Void>;
 }
