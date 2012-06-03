0bde62a59acde47f5a11d5f09e025870cc049136
tong <sdk@disktree.net>
2012-06-03 15:12:06 +0200

    added browsingData api

---
+++ chrome/BrowsingData.hx
@@ -0,0 +1,37 @@
+package chrome;
+
+typedef RemovalOptions = {
+	?since : Float
+}
+
+typedef DataToRemove = {
+	?appcache : Bool,
+	?cache : Bool,
+	?cookies : Bool,
+	?downloads : Bool,
+	?fileSystems : Bool,
+	?formData : Bool,
+	?history : Bool,
+	?indexedDB : Bool,
+	?localStorage : Bool,
+	?serverBoundCertificates : Bool,
+	?pluginData : Bool,
+	?passwords : Bool,
+	?webSQL : Bool
+}
+
+@:native("chrome.BrowsingData") extern class BrowsingData {
+	static function remove( options : RemovalOptions, dataToRemove : DataToRemove, ?cb : Void->Void ) : Void;
+	static function removeAppcache( options : RemovalOptions, ?cb : Void->Void ) : Void;
+	static function removeCache( options : RemovalOptions, ?cb : Void->Void ) : Void;
+	static function removeCookies( options : RemovalOptions, ?cb : Void->Void ) : Void;
+	static function removeDownloads( options : RemovalOptions, ?cb : Void->Void ) : Void;
+	static function removeFileSystems( options : RemovalOptions, ?cb : Void->Void ) : Void;
+	static function removeFormData( options : RemovalOptions, ?cb : Void->Void ) : Void;
+	static function removeHistory( options : RemovalOptions, ?cb : Void->Void ) : Void;
+	static function removeIndexedDB( options : RemovalOptions, ?cb : Void->Void ) : Void;
+	static function removeLocalStorage( options : RemovalOptions, ?cb : Void->Void ) : Void;
+	static function removePasswords( options : RemovalOptions, ?cb : Void->Void ) : Void;
+	static function removePluginData( options : RemovalOptions, ?cb : Void->Void ) : Void;
+	static function removeWebSQL( options : RemovalOptions, ?cb : Void->Void ) : Void;
+}
--- chrome/Event.hx
+++ chrome/Event.hx
@@ -1,7 +1,7 @@
 package chrome;
 
 typedef Event<T> = {
-	function addListener( f : T ) : Void;
+	function addListener( a : Dynamic, ?b : Dynamic, ?c : Dynamic, ?d : Dynamic, ?e : Dynamic ) : Void;
 	function removeListener( f : T ) : Void;
 	function hasListener( f : T ) : Void;
 }
