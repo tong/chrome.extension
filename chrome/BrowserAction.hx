5e5b095161b82a0219f9042b5a1b6c2d1fd12ba0
tong <sdk@disktree.net>
2012-08-17 10:00:18 +0200

    minor api updates

--- chrome/BrowserAction.hx
+++ chrome/BrowserAction.hx
@@ -12,7 +12,7 @@ package chrome;
 	
 	static function setBadgeBackgroundColor( details : { color : Array<Int>, ?tabId : Int } ) : Void;
 	static function setBadgeText( details : { text : String, ?tabId : Int } ) : Void;
-	static function setIcon( details : { ?imageData : Dynamic, ?path : String, ?tabId : Int } ) : Void; // ?imageData : ImageData
+	static function setIcon( details : { ?imageData : Dynamic, ?path : String, ?tabId : Int }, ?cb : Void->Void ) : Void; // ?imageData : ImageData
 	static function setPopup( details : { ?tabId : Int, popup : String } ) : Void;
 	static function setTitle( details : { title : String, ?tabId : Int } ) : Void;
 	
--- chrome/PageAction.hx
+++ chrome/PageAction.hx
@@ -4,29 +4,35 @@ package chrome;
 	http://code.google.com/chrome/extensions/pageAction.html
 */
 @:native("chrome.pageAction") extern class PageAction {
+	
 	static function getPopup( details : { tabId : Int }, cb : String->Void ) : Void;
 	static function getTitle( details : { tabId : Int }, cb : String->Void ) : Void;
 	static function show( tabId : Int ) : Void;
 	static function hide( tabId : Int ) : Void;
+	
 	static function setIcon(
 		details : {
 			tabId : Int,
 			?imageData : Dynamic, // TODO ImageData
 			?path : String,
 			?iconIndex : Int
-		}
+		},
+		?cb : Void->Void
 	) : Void;
+	
 	static function setPopup(
 		details : {
 			tabId : Int,
 			popup : String
 		}
 	) : Void;
+	
 	static function setTitle(
 		details : {
 			tabId : Int,
 			title : String
 		}
 	) : Void;
+	
 	static var onClicked(default,null) : Event<Tab->Void>;
 }
