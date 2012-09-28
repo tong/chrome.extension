d1948f28fd9775a292553d281e73375a5be75d4d
tong <sdk@disktree.net>
2012-09-28 10:38:44 +0200

    updated Alarms api

---
+++ chrome/Alarms.hx
@@ -0,0 +1,24 @@
+package chrome;
+
+typedef Alarm = {
+	?periodInMinutes : Float,
+	scheduledTime : Float,
+	name : String
+}
+
+typedef AlarmCreateInfo = {
+	?delayInSeconds : Float,
+	?periodInMinutes : Float,
+	?when : Float
+}
+
+@:native("chrome.alarms") extern class Alarms {
+
+	static function create( ?name : String, alarmInfo : AlarmCreateInfo ) : Void;
+	static function getAll( cb : Array<Alarm>->Void ) : Void;
+	static function clearAll() : Void;
+	static function clear( ?name : String ) : Void;
+	static function get( ?name : String, cb : Alarm->Void ) : Void;
+	
+	static var onAlarm : Event<Alarm->Void>;
+}
