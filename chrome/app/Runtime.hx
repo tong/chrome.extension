722e33e4095e3948a1eb96967a09fe7b2bc33014
tong <sdk@disktree.net>
2012-10-06 17:47:44 +0200

    added chronme.app.* classes

---
+++ chrome/app/Runtime.hx
@@ -0,0 +1,25 @@
+package chrome.app;
+
+typedef IntentResponse = {
+	var intentId : Int;
+	var data : Dynamic;
+	var success : Bool;
+}
+
+typedef Intent = {
+	var action : String;
+	var type : String;
+	var postResult : Void->Void; //TODO type?
+	var data : Dynamic;
+	var postFailure : Void->Void; //TODO type?
+}
+
+typedef LaunchData = {
+	var intent : Intent;
+}
+
+@:native("chrome.app.runtime")
+extern class Runtime {
+	static function onLaunched() : chrome.Event<LaunchData>;
+	
+}
