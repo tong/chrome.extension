922a6602b49a0a544a25dcaf515ecd1339220e8b
tong <sdk@disktree.net>
2012-10-08 00:48:37 +0200

    fix

--- chrome/PageCapture.hx
+++ chrome/PageCapture.hx
@@ -1,8 +1,8 @@
-package chrome.devtools;
+package chrome;
 
 /**
  * http://code.google.com/chrome/extensions/pageCapture.html
  */
-@:native("chrome.input.pageCapture") extern class PageCapture {
+@:native("chrome.pageCapture") extern class PageCapture {
 	static function saveAsMHTML( details : { tabId : Int, }, cb : Blob->Void ) : Void;
 }
