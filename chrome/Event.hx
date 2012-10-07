9a23a016ec643c68817b3b88c7dc9cdf83ee58b8
tong <sdk@disktree.net>
2012-10-08 00:49:16 +0200

    added missing methods to chrome.Event

--- chrome/Event.hx
+++ chrome/Event.hx
@@ -1,7 +1,40 @@
 package chrome;
 
+typedef UrlFilter = {
+	?schemes : Array<String>,
+	?pathContains : String,
+	?urlContains : String,
+	?hostPrefix : String,
+	?hostContains : String,
+	?hostSuffix : String,
+	?pathSuffix : String,
+	?urlPrefix : String,
+	?hostEquals : String,
+	?querySuffix : String,
+	?queryContains : String,
+	?pathPrefix : String,
+	?pathEquals : String,
+	?urlEquals : String,
+	?queryEquals : String,
+	?queryPrefix : String,
+	?urlSuffix : String,
+	?ports : Array<Int>
+}
+
+typedef Rule = {
+	?priority : Int,
+	conditions : Array<Dynamic>,
+	?id : String,
+	?actions : Array<Dynamic>
+}
+
 typedef Event<T> = {
+
 	function addListener( a : Dynamic, ?b : Dynamic, ?c : Dynamic, ?d : Dynamic, ?e : Dynamic ) : Void;
-	function removeListener( f : T ) : Void;
-	function hasListener( f : T ) : Void;
+	function removeListener( cb : T ) : Void;
+	function hasListener( cb : T ) : Void;
+	
+	function getRules( ?ruleIdentifiers : Array<String>, cb : Array<Rule>->Void ) : Void;
+	function removeRules( ?ruleIdentifiers : Array<String>, cb : Void->Void ) : Void;
+	function addRules( rules : Array<Rule>, ?cb : Array<Rule>->Void ) : Void;
 }
