fa585cb8fc7a6aa92f3601f84d1dcad3fbc17733
tong <sdk@disktree.net>
2012-12-20 16:57:54 +0100

    added missing onStart event listener to Runtime class, added doc

--- chrome/Runtime.hx
+++ chrome/Runtime.hx
@@ -9,14 +9,31 @@ typedef ManifestDetails = {
 
 @:native("chrome.runtime") extern class Runtime {
 	
+	/***/
 	static var lastError(default,null) : { ?message : String };
+	
+	/** The ID of the extension/app. */
 	static var id(default,null) : String;
 	
+	/** Retrieves the JavaScript 'window' object for the background page running inside the current extension. */
 	static function getBackgroundPage( cb : Dynamic->Void ) : Void;
+	
+	/** Returns details about the app or extension from the manifest. The object returned is a serialization of the full manifest file. */
 	static function getManifest( cb : ManifestDetails->Void ) : Void;
+	
+	/** Converts a relative path within an app/extension install directory to a fully-qualified URL. */
 	static function getURL( path : String ) : String;
 	
+	/** Fired when the browser first starts up */
+	static var onStartup : Event<Void->Void>;
+	
+	/** Fired when the extension is first installed, and on each update to a new version */
 	static var onInstalled : Event<Void->Void>;
+	
+	/** Sent to the event page just before it is unloaded. */
 	static var onSuspend : Event<Void->Void>;
+	
+	/** Sent after onSuspend() to indicate that the app won't be unloaded after all. */
 	static var onSuspendCanceled : Event<Void->Void>;
+	
 }
