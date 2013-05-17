b33762c8b9dbe1de90cef935e05eb77258628cee
tong <sdk@disktree.net>
2013-05-17 15:59:19 +0200

    updated and moved Downloads out of experimental

---
+++ chrome/Downloads.hx
@@ -0,0 +1,37 @@
+package chrome;
+
+//TODO full types
+
+@:fakeEnum(String)
+private enum DangerType {
+	file;
+	url;
+	content;
+	uncommon;
+	host;
+	safeM
+}
+
+@:fakeEnum(String)
+private enum State {
+	in_progress;
+	interrupted;
+	complete;
+}
+
+@:native("chrome.downloads") extern class Downloads {
+	static function download( options : Dynamic, ?f : Int->Void ) : Void;
+	static function search( query : Dynamic, f : Array<Dynamic>->Void ) : Void;
+	static function pause( downloadId : Int, ?f : Void->Void ) : Void;
+	static function resume( downloadId : Int, ?f : Void->Void ) : Void;
+	static function cancel( downloadId : Int, ?f : Void->Void ) : Void;
+	static function getFileIcon( downloadId : Int, ?options : Dynamic, f : String->Void ) : Void;
+	static function open( downloadId : Int ) : Void;
+	static function show( downloadId : Int ) : Void;
+	static function erase( query : Dynamic, ?f : Array<Int>->Void ) : Void;
+	static function acceptDanger( downloadId : Int ) : Void;
+	static function drag( downloadId : Int ) : Void;
+	static var onCreated : Event<Dynamic->Void>;
+	static var onErased : Event<Int->Void>;
+	static var onChanged : Event<Dynamic->Void>;
+}
