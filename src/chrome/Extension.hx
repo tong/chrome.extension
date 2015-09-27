82bc9cc9785c0bb2c18971ba8f92631a742de695
Tong <sdk@disktree.net>
2015-09-27 05:42:11 +0200

    Fix

--- src/chrome/Extension.hx
+++ src/chrome/Extension.hx
@@ -9,15 +9,12 @@ package chrome;
 @:require(chrome_ext)
 @:native("chrome.extension")
 extern class Extension {
-	static var lastError : { message : String };
-	static var inIncognitoContext : Bool;
-	//static function sendRequest( ?extensionId : String, request : Dynamic, ?f : Dynamic->Void ) : Void;
+	static var lastError(default,null) : { message : String };
+	static var inIncognitoContext(default,null) : Bool;
 	static function getURL( path : String ) : String;
 	static function getViews( ?fetchProperties : { ?type : ViewType, ?windowId : Int } ) : Array<js.html.Window>;
 	static function getBackgroundPage() : js.html.Window;
 	static function isAllowedIncognitoAccess( callback : Bool->Void ) : Void;
 	static function isAllowedFileSchemeAccess( callback : Bool->Void ) : Void;
 	static function setUpdateUrlData( data : String ) : Void;
-	//static var onRequest(default,null) : Event<Dynamic->MessageSender->(Dynamic->Void)->Void>;
-	//static var onRequestExternal(default,null) : Event<Dynamic->MessageSender->(Dynamic->Void)->Void>;
 }
