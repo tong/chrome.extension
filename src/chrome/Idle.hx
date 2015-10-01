ea5c2a91947c9d5432375ef06b20b47113705c01
Tong <sdk@disktree.net>
2015-10-01 20:00:58 +0200

    Fix

--- src/chrome/Idle.hx
+++ src/chrome/Idle.hx
@@ -13,5 +13,5 @@ import chrome.Events;
 extern class Idle {
 	static function queryState( detectionIntervalInSeconds : Int, callback : IdleState->Void ) : Void;
 	static function setDetectionInterval( intervalInSeconds : Int ) : Void;
-	static var onStateChanged : Event<IdleState->Void>;
+	static var onStateChanged(default,null) : Event<IdleState->Void>;
 }
