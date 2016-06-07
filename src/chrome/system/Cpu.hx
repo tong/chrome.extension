c8159627d680ea3d893bd4c53fe5be31412d8096
tong <sdk@disktree.net>
2016-06-07 17:39:48 +0200

    abstract string enum for Cpu.ProcessorInfo

--- src/chrome/system/Cpu.hx
+++ src/chrome/system/Cpu.hx
@@ -1,6 +1,6 @@
 package chrome.system;
 
-private typedef Processor = {
+typedef Processor = {
 	var usage : {
 		user : Float,
 		kernel : Float,
@@ -9,9 +9,19 @@ private typedef Processor = {
 	};
 }
 
+@:enum abstract ProcessorFeature(String) from String to String {
+	var mmx = "mmx";
+	var sse = "sse";
+	var sse2 = "sse2";
+	var sse3 = "sse3";
+	var sse4_1 = "sse4_1";
+	var sse4_2 = "sse4_2";
+	var avx = "avx";
+}
+
 @:require(chrome)
 @:native("chrome.system.cpu")
 extern class Cpu {
-	static function getInfo( callback : {numOfProcessors:Int,archName:String,modelName:String,features:Array<String>,processors:Array<Processor>}->Void ) : Void;
+	static function getInfo( callback : {numOfProcessors:Int,archName:String,modelName:String,features:Array<ProcessorFeature>,processors:Array<Processor>}->Void ) : Void;
 
 }
