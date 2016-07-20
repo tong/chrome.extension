7711fd5fceb2def01d485e5fd5ccbca9a594429f
tong <sdk@disktree.net>
2016-07-20 14:49:56 +0200

    Fix

--- src/chrome/devtools/Panels.hx
+++ src/chrome/devtools/Panels.hx
@@ -1,8 +1,11 @@
 package chrome.devtools;
 
-import js.html.DOMWindow;
 import chrome.Events;
 import chrome.devtools.InspectedWindow;
+
+typedef Window =
+	#if (haxe_ver >= 3.3) js.html.Window
+	#else js.html.DOMWindow #end;
 
 typedef ElementsPanel = {
 	function createSidebarPane( title : String, ?callback : ExtensionSidebarPane->Void ) : Void;
@@ -13,7 +16,7 @@ typedef ExtensionPanel = {
 	function createStatusBarButton( iconPath : String, tooltipText : String, disabled : Bool ) : Button;
 	var onHidden(default,null) : Event<Void->Void>;
 	var onSearch(default,null) : Event<String->String->Void>;
-	var onShown(default,null) : Event<DOMWindow->Void>;
+	var onShown(default,null) : Event<Window->Void>;
 }
 
 typedef ExtensionSidebarPane = {
@@ -22,7 +25,6 @@ typedef ExtensionSidebarPane = {
 	function setObject( jsonObject : String, ?rootTitle : String, ?callback : Void->Void ) : Void;
 	function setPage( path : String ) : Void;
 }
-
 
 private typedef Button = {
 	function update( ?iconPath : String, ?tooltipText : String, ?disabled : Bool ) : Void;
