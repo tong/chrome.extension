7db4d0f306243fbf7049bda3d1058d420fa37b4e
okawa-h <ookawa124@gmail.com>
2019-03-22 13:52:01 +0900

    add api at Panels

--- src/chrome/devtools/Panels.hx
+++ src/chrome/devtools/Panels.hx
@@ -12,6 +12,11 @@ typedef ElementsPanel = {
 	var onSelectionChanged(default,null) : Event<Void->Void>;
 }
 
+typedef SourcesPanel = {
+	function createSidebarPane( title : String, ?callback : ExtensionSidebarPane->Void ) : Void;
+	var onSelectionChanged(default,null) : Event<Void->Void>;
+}
+
 typedef ExtensionPanel = {
 	function createStatusBarButton( iconPath : String, tooltipText : String, disabled : Bool ) : Button;
 	var onHidden(default,null) : Event<Void->Void>;
@@ -28,13 +33,16 @@ typedef ExtensionSidebarPane = {
 
 private typedef Button = {
 	function update( ?iconPath : String, ?tooltipText : String, ?disabled : Bool ) : Void;
-	var onShown(default,null) : Event<Void->Void>;
+	var onClicked(default,null) : Event<Void->Void>;
 }
 
 @:require(chrome_ext)
 @:native("chrome.devtools.panels")
 extern class Panels {
 	static var elements : ElementsPanel;
+	static var sources : SourcesPanel;
+	static var themeName : String;
 	static function create( title : String, iconPath : String, pagePath : String, ?callback : ExtensionPanel->Void ) : Void;
 	static function setOpenResourceHandler( ?f : Resource->Void ) : Void;
+	static function openResource( url : String, lineNumber : Int, ?f : Void->Void ) : Void;
 }
