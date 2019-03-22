d21f5273afef74c731bb382068a7798ea781d592
okawa-h <ookawa124@gmail.com>
2019-03-22 16:19:10 +0900

    add api at Privacy

--- src/chrome/Privacy.hx
+++ src/chrome/Privacy.hx
@@ -2,10 +2,20 @@ package chrome;
 
 import chrome.Types;
 
+@:enum abstract IPHandlingPolicy(String) from String to String {
+	var default = "default";
+	var default_public_and_private_interfaces = "default_public_and_private_interfaces";
+	var default_public_interface_only = "default_public_interface_only";
+	var disable_non_proxied_udp = "disable_non_proxied_udp";
+}
+
 private typedef Services = {
 	alternateErrorPagesEnabled : ChromeSetting,
-	autofillEnabled : ChromeSetting,
+	autofillAddressEnabled : ChromeSetting,
+	autofillCreditCardEnabled : ChromeSetting,
+	passwordSavingEnabled : ChromeSetting,
 	safeBrowsingEnabled : ChromeSetting,
+	safeBrowsingExtendedReportingEnabled : ChromeSetting,
 	searchSuggestEnabled : ChromeSetting,
 	spellingServiceEnabled : ChromeSetting,
 	translationServiceEnabled : ChromeSetting
@@ -15,13 +25,14 @@ private typedef Websites = {
 	thirdPartyCookiesAllowed : ChromeSetting,
 	hyperlinkAuditingEnabled : ChromeSetting,
 	referrersEnabled : ChromeSetting,
+	doNotTrackEnabled : ChromeSetting,
 	protectedContentEnabled : ChromeSetting
 }
 
 @:require(chrome_ext)
 @:native("chrome.privacy")
 extern class Privacy {
-	static var network(default,never) : { networkPredictionEnabled : ChromeSetting, webRTCMultipleRoutesEnabled : Dynamic };
+	static var network(default,never) : { networkPredictionEnabled : ChromeSetting, webRTCIPHandlingPolicy : Dynamic };
 	static var services(default,never) : Services;
 	static var websites(default,never) : Websites;
 }
