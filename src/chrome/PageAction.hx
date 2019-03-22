d677cfa31489d1d493a93ae3c01aad216a98ffdf
okawa-h <ookawa124@gmail.com>
2019-03-22 16:01:21 +0900

    add api at PageAction

--- src/chrome/PageAction.hx
+++ src/chrome/PageAction.hx
@@ -7,12 +7,12 @@ import chrome.Tabs;
 @:require(chrome_ext)
 @:native("chrome.pageAction")
 extern class PageAction {
-	static function show( tabId : Int ) : Void;
-	static function hide( tabId : Int ) : Void;
-	static function setTitle( details : { tabId : Int, title : String } ) : Void;
+	static function show( tabId : Int, ?callback : Void->Void ) : Void;
+	static function hide( tabId : Int, ?callback : Void->Void ) : Void;
+	static function setTitle( details : { tabId : Int, title : String }, ?callback : Void->Void ) : Void;
 	static function getTitle( details : { tabId : Int }, callback : String->Void ) : Void;
 	static function setIcon( details : { tabId : Int, ?imageData : ImageData, ?path : Dynamic, ?iconIndex : Int }, ?callback : Void->Void ) : Void;
-	static function setPopup( details : { tabId : Int, popup : String } ) : Void;
+	static function setPopup( details : { tabId : Int, popup : String }, ?callback : Void->Void ) : Void;
 	static function getPopup( details : { tabId : Int }, callback : String->Void ) : Void;
 	static var onClicked(default,never) : Event<Tab->Void>;
 }
