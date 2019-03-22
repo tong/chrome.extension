c862dad60fc39e93371830b972369d297fd67c71
okawa-h <ookawa124@gmail.com>
2019-03-22 13:30:09 +0900

    add api at DesktopCapture

--- src/chrome/DesktopCapture.hx
+++ src/chrome/DesktopCapture.hx
@@ -6,11 +6,12 @@ import chrome.Tabs;
 	var screen = "screen";
 	var window = "window";
 	var tab = "tab";
+	var audio = "audio";
 }
 
 @:require(chrome_ext)
 @:native("chrome.desktopCapture")
 extern class DesktopCapture {
-	static function chooseDesktopMedia( sources : Array<DesktopCaptureSourceType>, ?targetTab : Tab, callback : String->Void ) : Void;
+	static function chooseDesktopMedia( sources : Array<DesktopCaptureSourceType>, ?targetTab : Tab, callback : { streamId : String,options : { canRequestAudioTrack:Bool } }->Void ) : Void;
 	static function cancelChooseDesktopMedia( desktopMediaRequestId : Int ) : Void;
 }
