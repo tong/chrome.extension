3392658b37aeda5f02f42562590d23da4f32ae7b
okawa-h <ookawa124@gmail.com>
2019-03-22 15:11:40 +0900

    add api at GCM

--- src/chrome/GCM.hx
+++ src/chrome/GCM.hx
@@ -9,7 +9,7 @@ extern class GCM {
 	static function register( senderIds : Array<String>, f : String->Void ) : Void;
 	static function unregister( f : Void->Void ) : Void;
 	static function send( message : {destinationId:String,messageId:String,?timeToLive:Int,data:Dynamic}, f : String->Void ) : Void;
-	static var onMessage(default,never) : Event<{data:Dynamic}->Void>;
+	static var onMessage(default,never) : Event<{data:Dynamic,?from:Strin,?collapseKey:String}->Void>;
 	static var onMessagesDeleted(default,never) : Event<Void->Void>;
 	static var onSendError(default,never) : Event<{errorMessage:String,?messageId:String,details:Dynamic}->Void>;
 }
