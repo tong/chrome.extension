6c548b6d359a229c0538a5de13a528393c3b5973
okawa-h <ookawa124@gmail.com>
2019-03-22 14:33:02 +0900

    add api at Extension

--- src/chrome/Extension.hx
+++ src/chrome/Extension.hx
@@ -2,17 +2,14 @@ package chrome;
 
 @:enum abstract ViewType(String) from String to String {
 	var tab = "tab";
-	var notification = "notification";
 	var popup = "popup";
 }
 
 @:require(chrome_ext)
 @:native("chrome.extension")
 extern class Extension {
-	static var lastError(default,never) : { message : String };
 	static var inIncognitoContext(default,never) : Bool;
-	static function getURL( path : String ) : String;
-	static function getViews( ?fetchProperties : { ?type : ViewType, ?windowId : Int } ) : Array<js.html.Window>;
+	static function getViews( ?fetchProperties : { ?type : ViewType, ?windowId : Int, ?tabId : Int } ) : Array<js.html.Window>;
 	static function getBackgroundPage() : js.html.Window;
 	static function isAllowedIncognitoAccess( callback : Bool->Void ) : Void;
 	static function isAllowedFileSchemeAccess( callback : Bool->Void ) : Void;
