ff9d48ae4b563cd0b70974249bedd32a611398cd
okawa-h <ookawa124@gmail.com>
2019-03-22 15:22:44 +0900

    add api at Idle

--- src/chrome/Idle.hx
+++ src/chrome/Idle.hx
@@ -13,5 +13,6 @@ import chrome.Events;
 extern class Idle {
 	static function queryState( detectionIntervalInSeconds : Int, callback : IdleState->Void ) : Void;
 	static function setDetectionInterval( intervalInSeconds : Int ) : Void;
+	static function getAutoLockDelay( callback : Int->Void ) : Void;
 	static var onStateChanged(default,never) : Event<IdleState->Void>;
 }
