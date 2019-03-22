a629b50d35d7245f0cfa921c619ee8047f85a989
okawa-h <ookawa124@gmail.com>
2019-03-22 13:35:37 +0900

    fix api at DesktopCapture

--- src/chrome/DesktopCapture.hx
+++ src/chrome/DesktopCapture.hx
@@ -12,6 +12,6 @@ import chrome.Tabs;
 @:require(chrome_ext)
 @:native("chrome.desktopCapture")
 extern class DesktopCapture {
-	static function chooseDesktopMedia( sources : Array<DesktopCaptureSourceType>, ?targetTab : Tab, callback : { streamId : String,options : { canRequestAudioTrack:Bool } }->Void ) : Void;
+	static function chooseDesktopMedia( sources : Array<DesktopCaptureSourceType>, ?targetTab : Tab, callback : String->{ canRequestAudioTrack:Bool }->Void ) : Void;
 	static function cancelChooseDesktopMedia( desktopMediaRequestId : Int ) : Void;
 }
