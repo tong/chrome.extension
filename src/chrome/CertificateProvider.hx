823607d9d177e4956f163ff376cf666f704b3180
okawa-h <ookawa124@gmail.com>
2019-03-22 11:38:30 +0900

    add api at CertificateProvider

--- src/chrome/CertificateProvider.hx
+++ src/chrome/CertificateProvider.hx
@@ -11,12 +11,25 @@ import chrome.Events;
 	var SHA512 = "SHA512";
 }
 
+@:enum abstract PinRequestErrorType(String) from String to String{
+	var INVALID_PIN = "INVALID_PIN";
+	var INVALID_PUK = "INVALID_PUK";
+	var MAX_ATTEMPTS_EXCEEDED = "MAX_ATTEMPTS_EXCEEDED";
+	var UNKNOWN_ERROR = "UNKNOWN_ERROR";
+}
+
+@:enum abstract RequestType(String) from String to String{
+	var PIN = "PIN";
+	var PUK = "PUK";
+}
+
 typedef CertificateInfo = {
     var certificate : ArrayBuffer;
     var supportedHashes : Array<Hash>;
 }
 
 typedef SignRequest = {
+    var signRequestId : Int;
     var digest : ArrayBuffer;
     var hash : Hash;
     var certificate : ArrayBuffer;
@@ -26,6 +39,16 @@ typedef SignRequest = {
 @:require(chrome_os)
 @:native("chrome.certificateProvider")
 extern class CertificateProvider {
+    static function requestPin(details : {
+        signRequestId : Int,
+        ?requestType : RequestType,
+        ?errorType : PinRequestErrorType,
+        ?attemptsLeft : Int
+    },callback : { userInput:String }->Void) : Void;
+    static function stopPinRequest(details : {
+        signRequestId : Int,
+        ?errorType : PinRequestErrorType,
+    },callback : Void->Void) : Void;
     static var onCertificatesRequested(default,never) : Event<(Array<CertificateInfo>->Void)->Void>;
     static var onSignDigestRequested(default,never) : Event<SignRequest->(ArrayBuffer->Void)->Void>;
 }
