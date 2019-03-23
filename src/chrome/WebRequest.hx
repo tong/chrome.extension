8f638632417fa52d50b1143214e34ab6b1de5e79
okawa-h <ookawa124@gmail.com>
2019-03-23 09:12:08 +0900

    add api at WebRequest

--- src/chrome/WebRequest.hx
+++ src/chrome/WebRequest.hx
@@ -1,5 +1,6 @@
 package chrome;
 
+import haxe.extern.EitherType;
 import chrome.Events;
 
 @:enum abstract ResourceType(String) from String to String {
@@ -8,8 +9,13 @@ import chrome.Events;
 	var stylesheet = "stylesheet";
 	var script = "script";
 	var image = "image";
+	var font = "font";
 	var object = "object";
 	var xmlhttprequest = "xmlhttprequest";
+	var ping = "ping";
+	var csp_report = "csp_report";
+	var media = "media";
+	var websocket = "websocket";
 	var other = "other";
 }
 
@@ -21,37 +27,44 @@ import chrome.Events;
 @:enum abstract OnBeforeSendHeadersOptions(String) from String to String {
 	var requestHeaders = "requestHeaders";
 	var blocking = "blocking";
+	var extraHeaders = "extraHeaders";
 }
 
 @:enum abstract OnSendHeadersOptions(String) from String to String {
 	//TODO missing in documentation
-	var blocking = "blocking";
+	var requestHeaders = "requestHeaders";
+	var extraHeaders = "extraHeaders";
 }
 
 @:enum abstract OnHeadersReceivedOptions(String) from String to String {
 	var blocking = "blocking";
 	var responseHeaders = "responseHeaders";
+	var extraHeaders = "extraHeaders";
 }
 
 @:enum abstract OnAuthRequiredOptions(String) from String to String {
 	var responseHeaders = "responseHeaders";
 	var blocking = "blocking";
 	var asyncBlocking = "asyncBlocking";
+	var extraHeaders = "extraHeaders";
 }
 
 @:enum abstract OnResponseStartedOptions(String) from String to String {
 	//TODO missing in documentation
 	var responseHeaders = "responseHeaders";
+	var extraHeaders = "extraHeaders";
 }
 
 @:enum abstract OnBeforeRedirectOptions(String) from String to String {
 	//TODO missing in documentation
 	var responseHeaders = "responseHeaders";
+	var extraHeaders = "extraHeaders";
 }
 
 @:enum abstract OnCompletedOptions(String) from String to String {
 	//TODO missing in documentation
 	var responseHeaders = "responseHeaders";
+	var extraHeaders = "extraHeaders";
 }
 
 typedef RequestFilter = {
@@ -68,7 +81,7 @@ typedef BlockingResponse = {
 	@:optional var redirectUrl : String;
 	@:optional var requestHeaders : HttpHeaders;
 	@:optional var responseHeaders : HttpHeaders;
-	@:optional var windauthCredentialsowId : Dynamic;
+	@:optional var authCredentials : {username:String,password:String};
 }
 
 typedef UploadData = {
@@ -76,11 +89,20 @@ typedef UploadData = {
 	@:optional var file : String;
 }
 
+typedef HttpHeaders = EitherType<Dynamic,String>;
+
+@:enum abstract IgnoredActionType(String) from String to String {
+	var redirect = "redirect";
+	var request_headers = "request_headers";
+	var response_headers = "response_headers";
+	var auth_credentials = "auth_credentials";
+}
+
 @:require(chrome_ext)
 @:native("chrome.webRequest")
 extern class WebRequest {
 	static var MAX_HANDLER_BEHAVIOR_CHANGED_CALLS_PER_10_MINUTES(default,never) : Int;
-	static function handlerBehaviorChanged( callback : Void->Void ) : Void;
+	static function handlerBehaviorChanged( ?callback : Void->Void ) : Void;
 	static var onBeforeRequest(default,never) : Event<{
 			requestId : String,
 			url : String,
@@ -94,7 +116,8 @@ extern class WebRequest {
 			},
 			tabId : Int,
 			type : ResourceType,
-			timeStamp : Float,
+			?initiator : String,
+			timeStamp : Float
 		}->Void>;
 	static var onBeforeSendHeaders(default,never) : Event<{
 			requestId : String,
@@ -103,7 +126,20 @@ extern class WebRequest {
 			frameId : Int,
 			parentFrameId : Int,
 			tabId : Int,
+			?initiator : String,
 			type : ResourceType,
+			timeStamp : Float,
+			?requestHeaders : HttpHeaders
+		}->Void>;
+	static var onSendHeaders(default,never) : Event<{
+			requestId : String,
+			url : String,
+			method : String,
+			frameId : Int,
+			parentFrameId : Int,
+			tabId : Int,
+			type : ResourceType,
+			?initiator : String,
 			timeStamp : Float,
 			?requestHeaders : HttpHeaders
 		}->Void>;
@@ -115,6 +151,7 @@ extern class WebRequest {
 			parentFrameId : Int,
 			tabId : Int,
 			type : ResourceType,
+			?initiator : String,
 			timeStamp : Float,
 			statusLine : String,
 			?responseHeaders : HttpHeaders,
@@ -128,10 +165,11 @@ extern class WebRequest {
 			parentFrameId : Int,
 			tabId : Int,
 			type : ResourceType,
+			?initiator : String,
 			timeStamp : Float,
 			scheme : String,
 			?realm : String,
-			?challenger : Dynamic,
+			challenger : {host:String,port:Int},
 			isProxy : Bool,
 			?responseHeaders : HttpHeaders,
 			statusLine : String,
@@ -145,9 +183,10 @@ extern class WebRequest {
 			parentFrameId : Int,
 			tabId : Int,
 			type : ResourceType,
+			?initiator : String,
 			timeStamp : Float,
 			?ip : String,
-			?fromCache : Bool,
+			fromCache : Bool,
 			statusCode : Int,
 			?responseHeaders : HttpHeaders,
 			statusLine : String
@@ -160,9 +199,10 @@ extern class WebRequest {
 			parentFrameId : Int,
 			tabId : Int,
 			type : ResourceType,
+			?initiator : String,
 			timeStamp : Float,
 			?ip : String,
-			?fromCache : Bool,
+			fromCache : Bool,
 			statusCode : Int,
 			redirectUrl : String,
 			?responseHeaders : HttpHeaders,
@@ -176,9 +216,10 @@ extern class WebRequest {
 			parentFrameId : Int,
 			tabId : Int,
 			type : ResourceType,
+			?initiator : String,
 			timeStamp : Float,
 			?ip : String,
-			?fromCache : Bool,
+			fromCache : Bool,
 			statusCode : Int,
 			?responseHeaders : HttpHeaders,
 			statusLine : String
@@ -191,9 +232,14 @@ extern class WebRequest {
 			parentFrameId : Int,
 			tabId : Int,
 			type : ResourceType,
+			?initiator : String,
 			timeStamp : Float,
 			?ip : String,
 			fromCache : Bool,
 			error : String
+		}->Void>;
+	static var onActionIgnored(default,never) : Event<{
+			requestId : String,
+			action : IgnoredActionType
 		}->Void>;
 }
